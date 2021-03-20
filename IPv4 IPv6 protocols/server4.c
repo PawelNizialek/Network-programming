dae3b4b5610522250c8d3224d7a2c1a391499f5b
PawelNizialek <62397625+PawelNizialek@users.noreply.github.com>
2021-03-20 22:17:05 +0100

    delete unnecessary comments

--- IPv4 IPv6 protocols/server4.c
+++ IPv4 IPv6 protocols/server4.c
@@ -9,14 +9,7 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/types.h>
-// #include <sys/select.h>
 
-void check(int returned_number){
-    if(returned_number==-1) {
-        fprintf(stdout, "error");
-        exit(-1);
-    }
-}
 int main(int argc, char** argv) {
     char buff[256];
     char addr_buff[256];
@@ -41,8 +34,6 @@ int main(int argc, char** argv) {
     server.sin_port = htons(atoi(argv[1]));
     // serwer moze odbierac dane z dowolnego interfejsu sieciowego
     server.sin_addr.s_addr = htonl(INADDR_ANY);;
-    // server.sin6_flowinfo = 
-    // server.sin6_scope_id = 
     if(bind(sockfd, (struct sockaddr*) &server, sizeof(struct sockaddr_in))==-1){
         fprintf(stdout, "bind()");
         exit(EXIT_FAILURE);
@@ -63,9 +54,6 @@ int main(int argc, char** argv) {
         fprintf(stdout, "Sending message\n");
         sleep(1);
         write(connfd, message, strlen(message));
-        // if(IN6_IS_ADDR_V4MAPPED(&server.sin_addr)){
-        //     fprintf(stdout, "\n%s is IPv4-mapped IPv6\n", inet_ntop(AF_INET, &server.sin_addr, addr_buff, sizeof(addr_buff)));
-        // }
         fprintf(stdout, "Closing client socket.\n");
         close(connfd);
     }
